0f445d66ef09841c541c70d36a128d4f943895df
arielsilva-tech <arielsilvasnt@hotmail.com>
2020-11-14 21:11:23 -0300

    Nivel 04. back-end do APP. Express Rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,7 +4,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
-
+import rateLimiter from '@shared/infra/http/middlewares/RateLimiter';
 import '@shared/infra/typeorm/index';
 import uploadConfig from '@config/upload';
 import AppError from '@shared/erros/AppError';
@@ -13,6 +13,7 @@ import routes from './routes';
 import '@shared/container';
 
 const app = express();
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
